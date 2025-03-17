530993d94ec7479df86e0083714950ef07a524b0
Anderson Lucas <114498251+andersoncsgo@users.noreply.github.com>
2025-03-17 19:37:45 +0000

    testeeee

---
+++ backend/src/main/java/com/example/teste/HelloController.java
@@ -0,0 +1,13 @@
+package com.exemplo.teste;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Olá do Spring Boot!";
+    }
+}
